fd73617d4375ccb0568d8116fc4bb93faef16ac7
Ricardo Beaujon <rbeaujon77@gmail.com>
2022-10-04 11:45:49 +0300

    styles Jokes, Activities, Dogs

--- src/screens/Activities/Activities.js
+++ src/screens/Activities/Activities.js
@@ -1,7 +1,8 @@
-import { View, Text } from 'react-native'
+import { View, Text, Image,  StyleSheet } from 'react-native'
 import React, {useState, useEffect} from 'react'
 import 'react-native-gesture-handler';
 import ApiActivities from '../../helper/Activities';
+import bg from '../../../assets/images/activities.jpg';
 
 
 const Activities = (props) => {
@@ -31,16 +32,96 @@ const Activities = (props) => {
 
 	const {activity, type, participants, price, link, key, accessibility} = activities
 	return (
-		<View>
-			<Text>{activity}</Text>
-			<Text>{type}</Text>
-			<Text>{participants}</Text>
-			<Text>{price}</Text>
-			<Text>{link}</Text>
-			<Text>{key}</Text>
-			<Text>{accessibility}</Text>
+		<View style={styles.container}>
+
+			<View>
+				<Image source={bg} style={styles.bg} />
+				<View style={styles.data}>
+					<Text style={styles.activityTitle}>Activity:</Text>
+					<Text style={styles.activity}>{activity}</Text>
+					<Text style={styles.type}>{type}</Text>
+					<Text style={styles.participants}>Participants: {participants}</Text>
+					<Text style={styles.price}>Price: {price}</Text>
+					{link ==="" ? "" : 	<Text style={styles.link}>Link: {link}</Text> }
+					<Text style={styles.key}>Key: {key}</Text>
+					<Text style={styles.accessibility}>{accessibility}</Text>
+				</View>
+			</View>
 		</View>
   )
 }
+
+const styles = StyleSheet.create({
+	container: {
+		width: '100%',
+		height: '100%',
+	  },
+	bg: {
+	 	width: '100%',
+	  	height: '100%',
+	  	justifyContent: 'center',
+	  	alignItems: 'center',
+	  	marginTop: '0%',
+		zIndex: 1,
+		elevation: 1,
+		position: 'relative',
+	},
+	data: {
+		padding:40,
+		zIndex: 100,
+		elevation: 100,
+		marginTop: 180,
+		position: 'absolute',
+	},
+	activityTitle: {
+		width: '100%',
+		textAlign: 'left',
+		fontSize: 20,
+		color: '#fff',
+	},
+	activity: {
+		width: '100%',
+		textAlign: 'left',
+		fontSize: 30,
+		color: '#fff',
+	},
+	type: {
+		width: '100%',
+		textAlign: 'left',
+		fontSize: 15,
+		color: '#dfd8d2',
+	},
+	participants: {
+		width: '100%',
+		textAlign: 'justify',
+		fontSize: 20,
+		marginTop: 20,
+		color: '#fff',
+	},
+	price: {
+		width: '100%',
+		textAlign: 'left',
+		fontSize: 20,
+		color: '#fff',
+	},
+	link: {
+		width: '100%',
+		textAlign: 'justify',
+		fontSize: 20,
+		color: '#fff'
+	},
+	key: {
+		width: '100%',
+		textAlign: 'left',
+		fontSize: 20,
+		color: '#fff',
+	},
+	accessibility: {
+		width: '100%',
+		textAlign: 'left',
+		fontSize: 20,
+		color: '#fff',
+	},
+  });
 
 export default Activities
--- src/screens/Home/Home.js
+++ src/screens/Home/Home.js
@@ -1,18 +1,22 @@
-import {View, Text, Button, StyleSheet, Pressable} from 'react-native'
+import { View, Text, Image,  StyleSheet } from 'react-native'
 import React from 'react'
 import CustomButton from '../../components/CustomButton';
 
 import 'react-native-gesture-handler';
 import InfoMenu from '../../navigation/InfoMenu/InfoMenu';
+import bg from '../../../assets/images/welcome.jpg';
 
 const Home = (props) => {
   return (
-	<View>
-		<InfoMenu  navigation={props.navigation}/>
-		<View  style={styles.container} >
+	<View style={styles.container}>
+
+		<View>
+			<InfoMenu style={styles.menu} navigation={props.navigation}/>
+			<Image source={bg} style={styles.bg} />
 			
-			<Text style={styles.container}>Home</Text>
-			
+			<View style={styles.data}>
+				<Text style={styles.info}>Some information can be here</Text>
+			</View>
 		</View>
 	</View>
   )
@@ -20,15 +24,45 @@ const Home = (props) => {
 
 const styles = StyleSheet.create({
 	container: {
-	  alignItems: 'center',
-	  marginTop: 20,
-	  padding: 50,
+		width: '100%',
+		height: '100%',
+		backgroundColor: '#fff'
+	},
+	bg: {
+		width: '100%',
+	 	height: '40%',
+		justifyContent: 'center',
+		alignItems: 'center',
+		marginTop: '100%',
+	   	zIndex: 1,
+	   	elevation: 1,
+	   	position: 'relative',
+   	},
+	menu: {
+		padding:40,
+		fontSize: 20,
+		position: 'absolute',
+	},
+   	data: {
+		padding:50,
+		zIndex: 100,
+		elevation: 100,
+		marginTop: 10,
+		position: 'absolute',
 	},
 	logo: {
 	  width: 250,
 	  height: 80,
 	  borderRadius: 15,
 	  marginBottom: 100,
+	},
+	info: {
+		width: '100%',
+		textAlign: 'left',
+		fontSize: 20,
+		marginTop: 100,
+		padding:10,
+		color: '#000',
 	},
   });
 
